9216a4134bef3541fd38b815c17dc33800d34955
Heath Stewart <heath.stewart1@gmail.com>
2021-03-26 08:07:52 -0400

    updated locate function

--- ip_geolocator.py
+++ ip_geolocator.py
@@ -1,13 +1,15 @@
-# Heath Stewart 20 Nov 2020
+# script by Sudo Hero 20 Nov 2020
 # get the geolocation of an IP address or a file containing a list of addresses
 # data gathered: lat/long coordinates, organization name, country, state, city, isp, and as number/organization
 # the data is queried from http://ip-api.com/json, see documentation at https://ip-api.com/docs/api:json#test
 # no api key is needed
 
 import requests
-import json
 import argparse
-import string
+from tabulate import tabulate
+from operator import itemgetter
+from time import sleep
+
 
 # define a function for the arguments we will accept, can be a single IP or a file with a list
 def get_args():
@@ -18,17 +20,23 @@ def get_args():
 
     return options
 
+
 options = get_args()
+located_list = []
+
 
 # define a function for geolocation given an IP address
 def locate(obj):
     ip = options.ip
-    response = requests.get(f"http://ip-api.com/json/{ip}" + "?fields=org,country,regionName,city,lat,lon,isp,as")      # store our request in response
-    ip_data = response.json()       # store the data in json format in ip_data
 
-    print(f'\n-=[ Location of {ip} ]=-')
-    for key, value in ip_data.items():
-        print(key, '=>', value)
+    # store our request in response
+    response = requests.get(f"http://ip-api.com/json/{ip}" + "?fields=org,country,regionName,city,lat,lon,isp,as")
+    resp_data = response.json()       # store the data in json format in resp_data
+    ip_data = {'ip_address': options.ip}    # create new dict for ip address
+    results = {**ip_data, **resp_data}      # create results dict with ip address first
+    located_list.append(results)
+    sleep(2)
+
 
 # define a function to handle the file processing if the '-f' option is used
 def process_file(file):
@@ -39,14 +47,24 @@ def process_file(file):
     with open(options.file, 'r') as f:
         lines = f.readlines()
         for line in lines:
-            ip_list.append(line.strip())    # each line has a new line char '\n', so we need to strip that for the input to be correct in locate() function
+            ip_list.append(line.strip())    # strip \n for the input to be correct in locate() function
         for i in ip_list:
             options.ip = i
             locate(options.ip)      # call the locate() function and get the geolocation data for the ip_list item
-    f.close()       # close the file
+
 
 # if the '-i' option for a single IP is used, call locate(), otherwise call process_file() for '-f' option
 if options.ip:
     locate(options.ip)
 elif options.file:
     process_file(options.file)
+
+# print the final results nicely in a table
+print(tabulate(sorted(located_list, key=itemgetter(
+        "ip_address",
+        "country",
+        "regionName",
+        "city",
+        "isp",
+        "org",
+        "as")), headers="keys"))
